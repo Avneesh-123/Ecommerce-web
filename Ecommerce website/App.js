1bc097e054842e242c8f5144e92c491ad785a010
Tan-123 <83863782+Avneesh-123@users.noreply.github.com>
2024-12-06 09:17:12 +0530

    Add files via upload

---
+++ Ecommerce website/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Login from './components/Login';
+import Checkout from './components/Checkout';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
